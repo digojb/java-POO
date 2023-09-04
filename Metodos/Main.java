d39cd71078e5a82d1d8cf9335b7cea458007fafa
Diogo José Bernardo <103767375+digojb@users.noreply.github.com>
2023-09-04 19:26:50 -0300

    first commit

---
+++ Metodos/Main.java
@@ -0,0 +1,12 @@
+package Metodos;
+
+public class Main {
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta();
+		c1.setModelo("BIC");
+		c1.setPonta(1.5f);
+		c1.status();
+		
+	}
+
+}
